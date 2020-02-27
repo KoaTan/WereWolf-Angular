84374605bf2a036197afdcb03d898a91acbb9774
khoa.td <khoa.td@itsstraining.edu.vn>
2020-02-27 10:31:59 +0700

    .

--- src/app/my-ui/playing-fiel/playing-fiel.component.ts
+++ src/app/my-ui/playing-fiel/playing-fiel.component.ts
@@ -10,8 +10,22 @@ export class PlayingFielComponent implements OnInit {
   
   
   constructor(public Router:Router) { 
-    
+    window.onscroll = function() {
+      changedaynight();
+    };
+  
+    function changedaynight() {
+      if (document.body.scrollHeight > 30|| document.documentElement.scrollHeight > 30) {
+          document.getElementById("borderPlayingFiel").className = "borderPlayingFielChange  ";
+        
+      } else {
+          document.getElementById("borderPlayingFiel").className = "borderPlayingFiel mat-toolbar mat-toolbar-multiple-rows";
+      
+      }
+    } 
   }
+    
+  
 
   ngOnInit(): void {
     
